6815358a948101d03179fe03f2917cce86e078a3
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2021-10-10 18:05:49 +0300

    Added a simple interface

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 const Game = (() =>
 {
-    //DOM attributes
-    let prompt = document.querySelector(".winner-prompt");
-    let overlay = document.querySelector(".overlay");
-
-    overlay.addEventListener("click", event =>
-    {
-        prompt.classList.remove("winner-prompt-active");
-        overlay.classList.remove("overlay-active");
-    });
-
-    //Inner factory functions.
-    const Player = (playerName, playerMark) =>
-    {
-        let mark = playerMark;
-        let name = playerName;
-
-        return {name, mark};
-    };
-    
+    //Inner factory functions.    
     const GameBoard = (() =>
     {
         //attrs
@@ -36,22 +18,47 @@ const Game = (() =>
         const cases = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
 
         //setup
-
-
         for(let i = 0; i < markers.length; i++)
         {
             markers[i].addEventListener("click", event =>
             {
                 if(markers[i].textContent == '')
                 {
-                    spots[i] = currentPlayer.mark;
+                    spots[i] = currentPlayer.marker;
                     markers[i].classList.add("marker-container-active");
-                    checkAndUpdate(i);
+                    let [result, marker] = checkWinner(i);
+
+                    if(result == true)
+                    {
+                        displayWinner(marker);
+                    }
+                    else
+                    {               
+                        if(!spots.includes(''))
+                        {
+                            displayWinner('tie');
+                            return;
+                        }
+
+                        if(secondPlayer.type == "random")
+                        {
+                            randomPlay();
+                        }
+                        else if(secondPlayer.type == "normal")
+                        {
+                            if(currentPlayer == secondPlayer)
+                                updateCurrent(firstPlayer);
+                            else
+                                updateCurrent(secondPlayer);
+                        }
+                    }
+                    updateDisplay();
                 }
             });
         }
 
         //methods
+
         const updateDisplay = () =>
         {
             for(let i = 0; i < spots.length; i++)
@@ -60,24 +67,7 @@ const Game = (() =>
             }
         };
 
-        const checkPossibleCases = (...possibleCases) =>
-        {
-            let result = false;
-            let marker = '';
-            //c stands for case
-            for(let c of possibleCases)
-            {
-                result = (spots[c[0]] == spots[c[1]] && spots[c[1]] == spots[c[2]]);
-                if(result == true)
-                {
-                    marker = spots[c[0]];
-                }
-            }
-
-            return [result, marker];
-        };
-
-        const checkAndUpdate = (index) =>
+        const checkWinner = (index) =>
         {
             let possibleCases = [];
             for(let singleCase of cases)
@@ -88,18 +78,45 @@ const Game = (() =>
                 }
             }
 
-            let [result, marker] = checkPossibleCases(...possibleCases);
-
-            if(result)
+            let result = false;
+            let p = '';
+            
+            for(let c of possibleCases)
             {
-                prompt.textContent = `The winner is: ${marker.toUpperCase()}.`;
-                prompt.classList.add("winner-prompt-active");
-                overlay.classList.add("overlay-active");
-                reInitialize();
+                result = (spots[c[0]] == spots[c[1]] && spots[c[1]] == spots[c[2]]);
+                if(result == true)
+                {
+                    if(spots[c[0]] == firstPlayer.marker)
+                    {
+                        p = firstPlayer.name;
+                        return [result, p];
+                    }
+                    else
+                    {
+                        p = secondPlayer.name;
+                        return [result, p];
+                    }
+                }
             }
-            updateDisplay()
-            updatePlayers();
+
+            return [result, p];
         };
+
+        const displayWinner = (marker) =>
+        {
+            if(marker == 'tie')
+            {
+                prompt.textContent = "A tie!";
+            }
+            else
+            {
+                prompt.textContent = `The winner is: ${marker}`;
+            }
+
+            prompt.classList.add("winner-prompt-active");
+            overlay.classList.add("overlay-active");
+            reInitialize();
+        }
 
         const reInitialize = () =>
         {
@@ -112,41 +129,166 @@ const Game = (() =>
             updateDisplay();
         };
 
-        return {spots, updateDisplay, reInitialize};
+        const randomMove = (marker) =>
+        {
+            let validSpots = [];
+            for(let i = 0; i < spots.length; i++)
+            {
+                if(spots[i] == '')
+                {
+                    validSpots.push(i);
+
+                    // spots[i] = marker;
+                    // markers[i].classList.add("marker-container-active");
+                    // updateDisplay();
+                    // return i;
+                }
+            }
+
+            let randSpot = validSpots[Math.floor(Math.random() * validSpots.length)];
+            spots[randSpot] = marker;
+            markers[randSpot].classList.add("marker-container-active");
+            updateDisplay();
+            return randSpot;
+        };
+
+        return {reInitialize, randomMove, checkWinner, displayWinner};
     })();
 
-
-    //attributes
-    const player1 = Player("Thoalfeqar", "x");
-    const player2 = Player("Other", "o");
-    let currentPlayer = player1;
     
-    //methods
-    const start = () =>
+    const Player = (playerName, playerMark) =>
     {
-        GameBoard.reInitialize();
+        let marker = playerMark;
+        let name = playerName;
+        let type = "normal";
+        return {name, marker, type};
     };
 
-    const updatePlayers = () =>
+    const RandomPlayer = (playerName, playerMark) =>
     {
-        if(currentPlayer == player1)
+        let player = Player(playerName, playerMark);
+        let type = "random";
+        let obj = Object.assign({}, player, {type});
+
+        return obj;
+    };
+
+    //attributes
+    let firstPlayer = Player("first", "O");
+    let secondPlayer = Player("second", "X");
+
+    //current player will be used if 2 players are playing against each other (humans).
+    let currentPlayer;
+
+
+    let markerChoice = 0;
+    let opponentChoice = 0;
+    
+    //DOM elements
+    const prompt = document.querySelector(".winner-prompt");
+    const overlay = document.querySelector(".overlay");
+    const welcomeScreen = document.querySelector(".welcome-screen");
+    const markerChoiceX = document.querySelector(".marker-choice-X");
+    const markerChoiceO = document.querySelector(".marker-choice-O");
+    const opponentOther = document.querySelector(".second-player-opponent");
+    const opponentRandom = document.querySelector(".randomAI-opponent");
+    const firstPlayerName = document.querySelector(".first-player-name");
+    const secondPlayerName = document.querySelector(".second-player-name");
+    const startButton = document.querySelector(".start-button");
+    const board = document.querySelector(".board-container");
+
+    markerChoiceX.addEventListener("click", event =>
+    {
+        event.target.classList.add("marker-hover");
+        markerChoiceO.classList.remove("marker-hover");
+        markerChoice = 'X';
+    });
+
+    markerChoiceO.addEventListener("click", event =>
+    {
+        event.target.classList.add("marker-hover");
+        markerChoiceX.classList.remove("marker-hover");
+        markerChoice = 'O';
+    });
+
+    opponentOther.addEventListener("click", event =>
+    {
+        event.target.classList.add("opponent-hover");
+        opponentRandom.classList.remove("opponent-hover");
+        opponentChoice = "normal";
+    });
+
+    opponentRandom.addEventListener("click", event =>
+    {
+        event.target.classList.add("opponent-hover");
+        opponentOther.classList.remove("opponent-hover");
+        opponentChoice = "random";
+    });
+
+    firstPlayerName.addEventListener("change", event =>
+    {
+        firstPlayer.name = event.target.value;
+    });
+
+    secondPlayerName.addEventListener("change", event => 
+    {
+        secondPlayer.name = event.target.value;
+    });
+
+    startButton.addEventListener("click", event =>
+    {
+        if(markerChoice != 0)
         {
-            currentPlayer = player2;
+            firstPlayer.marker = markerChoice;
         }
-        else
+        if(opponentChoice != 0)
         {
-            currentPlayer = player1;
+            if(opponentChoice == "random")
+            {
+                secondPlayer = RandomPlayer("random AI");
+                if(firstPlayer.marker == 'X')
+                {
+                    secondPlayer.marker = 'O';
+                }
+                else
+                {
+                    secondPlayer.marker = 'X';
+                }
+            }
+        }
+
+        currentPlayer = firstPlayer;
+        welcomeScreen.style.display = "none";
+        board.style.display = "flex";
+    });
+
+    overlay.addEventListener("click", event =>
+    {
+        prompt.classList.remove("winner-prompt-active");
+        overlay.classList.remove("overlay-active");
+    });
+
+
+    //methods
+    const randomPlay = () =>
+    {
+        let place = GameBoard.randomMove(secondPlayer.marker);
+        let [result, marker] = GameBoard.checkWinner(place);
+        
+        if(result == true)
+        {
+            GameBoard.displayWinner(marker);
         }
     }
 
-    const update = () =>
+    const updateCurrent = (to) =>
     {
-        
-    };
+        currentPlayer = to;
+    }
 
     const run = () =>
     {
-        start();
+        GameBoard.reInitialize();
     };
 
     return {run};
